0a7481048e414bcc625cd757d7fd022c4097320e
lamchiminhmark <34977592+lamchiminhmark@users.noreply.github.com>
2018-04-13 20:37:56 -0400

    Update WebPageIndex.java

--- lab7/WebPageIndex.java
+++ lab7/WebPageIndex.java
@@ -2,59 +2,93 @@ import java.util.*;
 import java.io.*;
 
 public class WebPageIndex {
-   
+   private RBTreeMap<String, List<Integer>> map;
+   private String baseUrl;
+   private int wordCount;
    //TODO: Insert the instance variables here
 
    // WebPageIndex constructor
-   WebPageIndex(String url) {
-      // TODO - implement me! - add some comments too!
+   WebPageIndex(String url) throws IOException{
+	   baseUrl = url;
+	   wordCount = 1;
+	   HTMLScanner input = new HTMLScanner(baseUrl);
+	   
+	   while (input.hasNext()) {
+		   String currentWord = input.next();
+		   
+		   // Finds the current values associated with the current word.
+		   List<Integer> currentValues = map.get(currentWord);
+		   // If there are no current values, make a new list and put in the first one.
+		   if (currentValues == null) {
+			   List<Integer> posList = new LinkedList<Integer>();
+			   posList.add(wordCount);
+			   map.put(currentWord, posList);
+			   // Otherwise, add the new value to the existing list.
+		   } else {
+			   currentValues.add(wordCount);
+		   }
+		   
+		   wordCount++;
+	   }
    }     
    
    // Returns a count of the words in this web page
    public int getWordCount() {
-      // TODO - implement me! - add some comments too!
-      return -1;
+	   //Simply returns the field wordCount.
+      return wordCount;
    }
    
    public String getUrl() {
-      // TODO - implement me! - add some comments too!
-      return "";
+      // Simply returns the field baseUrl.
+      return baseUrl;
    }
    
    public boolean contains(String s) {
-      // TODO - implement me! - add some comments too!
-      return false;
+	   // Returns true if the word is found in the map.
+	   if (map.get(s) != null) return true;
+	   return false;
    }
    
    public int getCount(String s) {
-      // TODO - implement me! - add some comments too!
-      return -1;
+	   List<Integer> wordList = map.get(s);
+	   if (wordList != null) {
+		   return wordList.size();
+	   }
+	   return 0;
    }
    
-   public double getFrequency(String s) {
-      // TODO - implement me! - add some comments too!
-      return -1.0;
+   public double getFrequency(String s) {	   
+	   int count = getCount(s);
+	   return ((double) count) / wordCount;
    }
    
    public List<Integer> getLocations(String s) {
-      // TODO - implement me! - add some comments too!
-      return null;
+	   // Return the list stored in the map if the word is found, 
+	   // Else return empty list.
+      List<Integer> wordList = map.get(s);
+      if (wordList != null) return wordList;
+      return new LinkedList<Integer>();
    }
 
    // return an Iterator over all the words in the index
    public Iterator<String> words() {
-      // TODO - implement me! - add some comments too!
-      return null;
+      return map.keys();
    }
    
    public String toString() {
-      // TODO - implement me! - add some comments too!
-      return "";
+      return map.toString();
    }
 
    // The main method is an application using a WebPageIndex
    public static void main(String[] args) throws IOException {
-      // TODO - implement me! - add some comments too!
+	   WebPageIndex input = new WebPageIndex(args[0]);
+	   Iterator<String> words = input.words();
+	   while (words.hasNext()) {
+		   String word = words.next();
+		   System.out.print(word + " ");
+		   System.out.print(input.getFrequency(word) + " ");
+		   System.out.println(input.getLocations(word));
+	   }
    }
 
    
